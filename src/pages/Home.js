281c9b9010cc6d7c0621352c79bc2c252785eced
patrickbatungbacal <patrickbatungbacal@gmail.com>
2021-01-26 21:46:01 +0800

    9. Applying Global Styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import Game from "../components/Game";
 //Styling and animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { newGamesURL } from "../api";
 
 const Home = () => {
   //GET DATA FROM STATE
@@ -27,6 +28,32 @@ const Home = () => {
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+
+      <h2>Popular Games</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
           <Game
             name={game.name}
             released={game.released}
